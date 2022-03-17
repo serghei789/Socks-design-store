d16ed6e890f0e1f92e072c6eef14674e947a6f82
Maxim <maxim_dyakonov@mail.ru>
2022-03-17 19:51:00 +0300

    favouritesFIRSTrelease

--- app.js
+++ app.js
@@ -9,11 +9,11 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 dotenv.config();
 
-const mainRouter = require('./src/routes/main');
-const registrationRouter = require('./src/routes/registration');
-const authorizationRouter = require('./src/routes/authorization');
-const generatorRouter = require('./src/routes/generator');
-const cartRouter = require('./src/routes/cart');
+// const mainRouter = require('./src/routes/main');
+// const registrationRouter = require('./src/routes/registration');
+// const authorizationRouter = require('./src/routes/authorization');
+// const generatorRouter = require('./src/routes/generator');
+// const cartRouter = require('./src/routes/cart');
 const favouritesRouter = require('./src/routes/favourites');
 
 app.set('views', path.join(process.env.PWD, 'src', 'views'));
@@ -35,11 +35,11 @@ const sessionConfig = {
   cookie: { expires: 24 * 60 * 60e3 },
 };
 
-app.use('/', mainRouter);
-app.use('/registration', registrationRouter);
-app.use('/authorization', authorizationRouter);
-app.use('/generator', generatorRouter);
-app.use('/cart', cartRouter);
+// app.use('/', mainRouter);
+// app.use('/registration', registrationRouter);
+// app.use('/authorization', authorizationRouter);
+// app.use('/generator', generatorRouter);
+// app.use('/cart', cartRouter);
 app.use('/favourites', favouritesRouter);
 
 app.use(session(sessionConfig));
---
+++ src/routes/favourites.js
@@ -0,0 +1,56 @@
+const express = require('express');
+
+const router = express.Router();
+
+const {
+  Favourite, Sockstype, Color, Ornament, Picture,
+} = require('../../db/models');
+
+router.get('/', (req, res) => {
+  res.render('favourites');
+});
+
+router.get('/favourites', async (req, res) => {
+  const designsFavUser = await Favourite.findAll({ where: { userId: req.session.userId }, raw: true });
+  const designArr = await Promise.all(designsFavUser.map(async (favourite) => {
+    const resDes = await Sockstype.findOne({ where: { id: favourite.socksId }, raw: true });
+    return resDes;
+  }));
+  const result = await Promise.all(designArr.map(async (designObj) => {
+    const { dataValues: color } = await Color.findOne({ where: { id: designObj.colorId } });
+    const { dataValues: ornament } = await Ornament.findOne({ where: { id: designObj.ornamentId } });
+    const { dataValues: picture } = await Picture.findOne({ where: { id: designObj.pictureId } });
+    return {
+      color: color.color, ornament: ornament.url, picture: picture.url, socksId: designObj.id,
+    };
+  }));
+  const getFavourite = await Favourite.findAll({ where: { userId: req.session.userId } });
+  res.render('favourites', { getFavourite, result });
+});
+
+router.delete('/delete/:id', async (req, res) => {
+  try {
+    await Favourite.destroy({ where: { id: req.params.id } });
+    res.sendStatus(200);
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(500);
+  }
+});
+
+router.get('/sockstype/:id', async (req, res) => {
+  // отрисовать методом из конструктора + добавить кнопку поделиться
+  const { dataValues } = await Favourite.findOne({ where: { id: req.params.id } });
+  // в клиентском js заглушить кнопку и сделать всплывающий инпут с ссылкой
+  // console.log(dataValues);
+  const { dataValues: sockstype } = await Sockstype.findOne({ where: { id: dataValues.socksId } });
+  const { dataValues: color } = await Color.findOne({ where: { id: sockstype.colorIid } });
+  const { dataValues: ornament } = await Ornament.findOne({ where: { id: sockstype.ornamentId } });
+  const { dataValues: picture } = await Picture.findOne({ where: { id: sockstype.pictureId } });
+  // console.log(color.color);
+  res.render('sockstype', {
+    favourite: dataValues, color: color.color, ornament: ornament.url, picture: picture.url,
+  });
+});
+
+module.exports = router;
