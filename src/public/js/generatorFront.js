a8fc092936728839223a89750cb89adfcb92e397
Archi <stepin711@gmail.com>
2022-03-18 14:40:35 +0300

    add pictures on socks

--- src/public/js/generatorFront.js
+++ src/public/js/generatorFront.js
@@ -1,35 +1,36 @@
 const colorSelector = document.querySelector('#colorSelector');
+const pictureSelector = document.querySelector('#pictureSelector');
 const socksDiv = document.querySelector('#socks');
-const colors = [{
-  id: 1, name: 'Skyblue', code: 'rgb(200, 226, 234)', createdAt: '2022-03-17T17:10:42.646Z', updatedAt: '2022-03-17T17:10:42.646Z',
-}, {
-  id: 2, name: 'Lightpink', code: 'rgb(242, 209 ,209)', createdAt: '2022-03-17T17:10:42.646Z', updatedAt: '2022-03-17T17:10:42.646Z',
-}, {
-  id: 3, name: 'Pink', code: 'rgb(198, 20, 78)', createdAt: '2022-03-17T17:10:42.646Z', updatedAt: '2022-03-17T17:10:42.646Z',
-}, {
-  id: 4, name: 'Navy blue', code: 'rgb(21, 53, 92)', createdAt: '2022-03-17T17:10:42.646Z', updatedAt: '2022-03-17T17:10:42.646Z',
-}, {
-  id: 5, name: 'Yellow', code: 'rgb(244, 191, 77)', createdAt: '2022-03-17T17:10:42.646Z', updatedAt: '2022-03-17T17:10:42.646Z',
-}, {
-  id: 6, name: 'Purple', code: 'rgb(106, 14, 111)', createdAt: '2022-03-17T17:10:42.646Z', updatedAt: '2022-03-17T17:10:42.646Z',
-}, {
-  id: 7, name: 'Red', code: 'rgb(192, 21, 24)', createdAt: '2022-03-17T17:10:42.646Z', updatedAt: '2022-03-17T17:10:42.646Z',
-}, {
-  id: 8, name: 'Green', code: 'rgb(33, 81, 93)', createdAt: '2022-03-17T17:10:42.646Z', updatedAt: '2022-03-17T17:10:42.646Z',
-}, {
-  id: 9, name: 'Brown', code: 'rgb(85, 56, 37)', createdAt: '2022-03-17T17:10:42.646Z', updatedAt: '2022-03-17T17:10:42.646Z',
-}, {
-  id: 10, name: 'Blue', code: 'rgb(106, 135, 167)', createdAt: '2022-03-17T17:10:42.646Z', updatedAt: '2022-03-17T17:10:42.646Z',
-}];
-console.log(colorSelector);
+const pictureOnSock1 = document.querySelector('#pictureOnSock1');
+const pictureOnSock2 = document.querySelector('#pictureOnSock2');
+
 colorSelector.addEventListener('click', async (e) => {
-  console.log(colorSelector.value);
-  console.log(socksDiv);
-  console.log(socksDiv.style.backgroundColor);
-  for (const color of colors) {
-    if (color.id === Number(colorSelector.value)) {
-      socksDiv.style.backgroundColor = color.code;
-    }
-  }
-  // socksDiv.style.backgroundColor = 'rgb(200, 226, 234)';
+  const id = e.target.value;
+  if (id === 'цвет') return;
+  // console.log(e.target.value);
+  const resp = await fetch('/generator', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ id }),
+  });
+  const code = await resp.json();
+  socksDiv.style.backgroundColor = code.code;
+});
+
+pictureSelector.addEventListener('click', async (e) => {
+  const id = e.target.value;
+  console.log(id);
+  if (id === 'картинка') return;
+  const resp = await fetch('/generator/picture', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ id }),
+  });
+  const src = await resp.json();
+  pictureOnSock1.style.backgroundImage = `url('${src.src}')`;
+  pictureOnSock2.style.backgroundImage = `url('${src.src}')`;
 });
