15ff410d7a07457c8a3d8c8f10975aa2c1806423
Serghei <78849747+serghei789@users.noreply.github.com>
2022-03-18 05:01:03 +0300

    updated database.js

--- db/database.js
+++ db/database.js
@@ -2,11 +2,12 @@ require('dotenv').config();
 
 module.exports = {
   "development": {
-    "username": process.env.DATABASE_USER,
-    "password": process.env.DATABASE_PASSWORD,
-    "database": process.env.DATABASE_NAME,
-    "host": "127.0.0.1",
-    "dialect": "postgres"
+    "use_env_variable":"DATABASE_URL",
+    "dialectOptions": {
+      "ssl": {
+        "rejectUnauthorized": false
+      }
+    }
   },
   "test": {
     "username": "root",
@@ -16,10 +17,16 @@ module.exports = {
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+    "use_env_variable":"DATABASE_URL",
+    "dialectOptions": {
+      "ssl": {
+        "rejectUnauthorized": false
+      }
+    }
   }
 }
+/* "username": process.env.DATABASE_USER,
+    "password": process.env.DATABASE_PASSWORD,
+    "database": process.env.DATABASE_NAME,
+    "host": "127.0.0.1",
+    "dialect": "postgres" */
