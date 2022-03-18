f3a50cbf0bf74bce780eb826a5fcf5ebdfa34b56
Archi <stepin711@gmail.com>
2022-03-18 10:42:38 +0300

    fixed bugs

--- app.js
+++ app.js
@@ -4,23 +4,23 @@ const hbs = require('hbs');
 const path = require('path');
 const session = require('express-session');
 const FileStore = require('session-file-store')(session);
-
-const app = express();
 const { addToLocals } = require('./src/middlewares/addToLocals');
 
+const app = express();
 const PORT = process.env.PORT || 4000;
 dotenv.config();
 
-const mainRouter = require('./src/routes/main');
-const registrationRouter = require('./src/routes/registration');
-const authorizationRouter = require('./src/routes/authorization');
-const generatorRouter = require('./src/routes/generator');
+// const mainRouter = require('./src/routes/main');
+// const registrationRouter = require('./src/routes/registration');
+// const authorizationRouter = require('./src/routes/authorization');
+// const generatorRouter = require('./src/routes/generator');
 const cartRouter = require('./src/routes/cart');
 const favouritesRouter = require('./src/routes/favourites');
 
+const registrationRout = require('./src/routes/registration_Rout');
+const authorizationRout = require('./src/routes/authorization_Rout');
+
 const index = require('./src/routes/index');
-const registration_Rout = require('./src/routes/registration_Rout');
-const authorization_Rout = require('./src/routes/authorization_Rout');
 
 hbs.registerPartials(path.join(process.env.PWD, 'src', 'views', 'partials'));
 app.set('views', path.join(process.env.PWD, 'src', 'views'));
@@ -34,7 +34,7 @@ hbs.registerPartials(path.join(process.env.PWD, 'src', 'views', 'partials'));
 
 const sessionConfig = {
   store: new FileStore(),
-  key: 'sid',
+  key: 'ourCookie',
   secret: process.env.SECRET,
   resave: false,
   saveUninitialized: false,
@@ -42,18 +42,19 @@ const sessionConfig = {
   cookie: { expires: 24 * 60 * 60e3 },
 };
 
-app.use('/', mainRouter);
-app.use('/registration', registrationRouter);
-app.use('/authorization', authorizationRouter);
-app.use('/generator', generatorRouter);
-app.use('/cart', cartRouter);
-app.use('/favourites', favouritesRouter);
-
 app.use(session(sessionConfig));
 app.use(addToLocals);
+
+// app.use('/', mainRouter);
+// app.use('/registration', registrationRouter);
+// app.use('/authorization', authorizationRouter);
+// app.use('/generator', generatorRouter);
+app.use('/favourites', favouritesRouter);
+app.use('/cart', cartRouter);
+app.use('/registration', registrationRout);
+app.use('/authorization', authorizationRout);
+
 app.use('/', index);
-app.use('/registration', registration_Rout);
-app.use('/authorization', authorization_Rout);
 
 app.listen(PORT, () => {
   console.log(`server started on PORT: ${PORT}`);
