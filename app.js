b549ae33eb30bc2daf7092470eab521f5bb435c4
Archi <stepin711@gmail.com>
2022-03-18 10:03:48 +0300

    add selected form on generator page

--- app.js
+++ app.js
@@ -7,19 +7,20 @@ const FileStore = require('session-file-store')(session);
 
 const app = express();
 const { addToLocals } = require('./src/middlewares/addToLocals');
+
 const PORT = process.env.PORT || 4000;
 dotenv.config();
 
-const mainRouter = require('./src/routes/main');
-const registrationRouter = require('./src/routes/registration');
-const authorizationRouter = require('./src/routes/authorization');
+// const mainRouter = require('./src/routes/main');
+// const registrationRouter = require('./src/routes/registration');
+// const authorizationRouter = require('./src/routes/authorization');
 const generatorRouter = require('./src/routes/generator');
-const cartRouter = require('./src/routes/cart');
-const favouritesRouter = require('./src/routes/favourites');
+// const cartRouter = require('./src/routes/cart');
+// const favouritesRouter = require('./src/routes/favourites');
 
-const index = require('./src/routes/index')
-const registration_Rout = require('./src/routes/registration_Rout');
-const authorization_Rout = require('./src/routes/authorization_Rout')
+// const index = require('./src/routes/index');
+// const registration_Rout = require('./src/routes/registration_Rout');
+// const authorization_Rout = require('./src/routes/authorization_Rout');
 
 hbs.registerPartials(path.join(process.env.PWD, 'src', 'views', 'partials'));
 app.set('views', path.join(process.env.PWD, 'src', 'views'));
@@ -41,19 +42,18 @@ const sessionConfig = {
   cookie: { expires: 24 * 60 * 60e3 },
 };
 
-app.use('/', mainRouter);
-app.use('/registration', registrationRouter);
-app.use('/authorization', authorizationRouter);
+// app.use('/', mainRouter);
+// app.use('/registration', registrationRouter);
+// app.use('/authorization', authorizationRouter);
 app.use('/generator', generatorRouter);
-app.use('/cart', cartRouter);
-app.use('/favourites', favouritesRouter);
+// app.use('/cart', cartRouter);
+// app.use('/favourites', favouritesRouter);
 
-app.use(session(sessionConfig));
-app.use(addToLocals);
-app.use('/', index);
-app.use('/registration',registration_Rout);
-app.use('/authorization', authorization_Rout);
-
+// app.use(session(sessionConfig));
+// app.use(addToLocals);
+// app.use('/', index);
+// app.use('/registration', registration_Rout);
+// app.use('/authorization', authorization_Rout);
 
 app.listen(PORT, () => {
   console.log(`server started on PORT: ${PORT}`);
--- src/routes/generator.js
+++ src/routes/generator.js
@@ -1,9 +1,17 @@
 const router = require('express').Router();
-const { Color } = require('../../db/models');
+const { Color, Ornament, Picture } = require('../../db/models');
 
+let colors = [];
 router.get('/', async (req, res) => {
-  const colors = await Color.findAll();
-  res.render('generator', { colors });
+  colors = await Color.findAll();
+  ornaments = await Ornament.findAll();
+  pictures = await Picture.findAll();
+  res.render('generator', { colors, ornaments, pictures });
 });
+/* router.post('/getcolors', async (req, res) => {
+  const { id } = req.body;
+  const color = await Color.findOne({ where: { id } });
+  res.send({ code: color.code });
+}); */
 
 module.exports = router;
